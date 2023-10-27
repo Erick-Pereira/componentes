5ac21013eac518ad2c8a337558cfc15b2f83a6e9
ERICK BAPTISTA PEREIRA <erick.pereira@alunos.sc.senac.br>
2023-10-27 19:30:27 -0300

    First commit

---
+++ src/components/NoticiasEsportivas.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps(['title','news','rate'])
+defineEmits('aumentarFonte','diminuirFonte')
+
+function aumentarFont(){
+emit('aumentarFonte')
+}
+function diminuirFont(){
+emit('diminuirFonte')
+}
+</script>
+<template>
+<div class="noticia">
+<h4>{{ title }}</h4>
+<p>{{ news }}</p>
+<p>{{ rate }}</p>
+<button @click="aumentarFont()">+</button>
+<button @click="diminuirFont()">-</button>
+</div>
+</template>
+<style scoped>
+.noticia{
+    border: 1px solid silver;
+    background-color: aqua;
+    margin: 10px;
+}
+</style>
---
+++ src/components/TituloPrincipal.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+defineProps(['title'])
+</script>
+<template>
+<h1>{{ title }}</h1>
+</template>
+<style>
+
+</style>
